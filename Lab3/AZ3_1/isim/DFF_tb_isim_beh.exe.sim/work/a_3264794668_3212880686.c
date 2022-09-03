59991f8a14281aaaf2395e5f9ca091ab8d95e59e
Sara Tajernia <61985583+sara-tajernia@users.noreply.github.com>
2022-09-03 16:24:57 +0430

    Add files via upload

---
+++ Lab3/AZ3_1/isim/DFF_tb_isim_beh.exe.sim/work/a_3264794668_3212880686.c
@@ -0,0 +1,111 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "S:/uni/4/Az memar/session 3/AZ3_1/DFF.vhd";
+extern char *IEEE_P_2592010699;
+
+unsigned char ieee_p_2592010699_sub_1690584930_503743352(char *, unsigned char );
+
+
+static void work_a_3264794668_3212880686_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    unsigned char t9;
+    unsigned char t10;
+    unsigned char t11;
+    unsigned char t12;
+    char *t13;
+    char *t14;
+
+LAB0:    xsi_set_current_line(45, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)2);
+    if (t4 != 0)
+        goto LAB2;
+
+LAB4:    xsi_set_current_line(48, ng0);
+    t1 = (t0 + 1152U);
+    t4 = xsi_signal_has_event(t1);
+    if (t4 == 1)
+        goto LAB8;
+
+LAB9:    t3 = (unsigned char)0;
+
+LAB10:    if (t3 != 0)
+        goto LAB5;
+
+LAB7:
+LAB6:
+LAB3:    t1 = (t0 + 2992);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(46, ng0);
+    t1 = (t0 + 3072);
+    t5 = (t1 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    *((unsigned char *)t8) = (unsigned char)3;
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB3;
+
+LAB5:    xsi_set_current_line(49, ng0);
+    t2 = (t0 + 1032U);
+    t6 = *((char **)t2);
+    t11 = *((unsigned char *)t6);
+    t12 = ieee_p_2592010699_sub_1690584930_503743352(IEEE_P_2592010699, t11);
+    t2 = (t0 + 3072);
+    t7 = (t2 + 56U);
+    t8 = *((char **)t7);
+    t13 = (t8 + 56U);
+    t14 = *((char **)t13);
+    *((unsigned char *)t14) = t12;
+    xsi_driver_first_trans_fast_port(t2);
+    goto LAB6;
+
+LAB8:    t2 = (t0 + 1192U);
+    t5 = *((char **)t2);
+    t9 = *((unsigned char *)t5);
+    t10 = (t9 == (unsigned char)3);
+    t3 = t10;
+    goto LAB10;
+
+}
+
+
+extern void work_a_3264794668_3212880686_init()
+{
+	static char *pe[] = {(void *)work_a_3264794668_3212880686_p_0};
+	xsi_register_didat("work_a_3264794668_3212880686", "isim/DFF_tb_isim_beh.exe.sim/work/a_3264794668_3212880686.didat");
+	xsi_register_executes(pe);
+}
