c069fb76ed7844185a4091100d8eda4391bc2cbb
Sara Tajernia <61985583+sara-tajernia@users.noreply.github.com>
2022-09-04 10:01:19 +0430

    Add files via upload

---
+++ Lab5/aztafrigh/isim/Ripple_Adder_tb_isim_beh.exe.sim/work/a_1773838443_2372691052.c
@@ -0,0 +1,215 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "S:/uni/4/Az memar/aztafrigh/Ripple_Adder_tb.vhd";
+
+
+
+static void work_a_1773838443_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    int64 t3;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+
+LAB0:    t1 = (t0 + 2672U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(81, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2480);
+    xsi_process_wait(t2, t3);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(82, ng0);
+    t2 = (t0 + 4973);
+    t5 = (t0 + 3056);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(83, ng0);
+    t2 = (t0 + 4977);
+    t5 = (t0 + 3120);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(87, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2480);
+    xsi_process_wait(t2, t3);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    xsi_set_current_line(88, ng0);
+    t2 = (t0 + 4981);
+    t5 = (t0 + 3056);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(89, ng0);
+    t2 = (t0 + 4985);
+    t5 = (t0 + 3120);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(91, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2480);
+    xsi_process_wait(t2, t3);
+
+LAB14:    *((char **)t1) = &&LAB15;
+    goto LAB1;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+LAB12:    xsi_set_current_line(92, ng0);
+    t2 = (t0 + 4989);
+    t5 = (t0 + 3056);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(93, ng0);
+    t2 = (t0 + 4993);
+    t5 = (t0 + 3120);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(95, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2480);
+    xsi_process_wait(t2, t3);
+
+LAB18:    *((char **)t1) = &&LAB19;
+    goto LAB1;
+
+LAB13:    goto LAB12;
+
+LAB15:    goto LAB13;
+
+LAB16:    xsi_set_current_line(96, ng0);
+    t2 = (t0 + 4997);
+    t5 = (t0 + 3056);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(97, ng0);
+    t2 = (t0 + 5001);
+    t5 = (t0 + 3120);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(99, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2480);
+    xsi_process_wait(t2, t3);
+
+LAB22:    *((char **)t1) = &&LAB23;
+    goto LAB1;
+
+LAB17:    goto LAB16;
+
+LAB19:    goto LAB17;
+
+LAB20:    xsi_set_current_line(100, ng0);
+    t2 = (t0 + 5005);
+    t5 = (t0 + 3056);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(101, ng0);
+    t2 = (t0 + 5009);
+    t5 = (t0 + 3120);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(103, ng0);
+
+LAB26:    *((char **)t1) = &&LAB27;
+    goto LAB1;
+
+LAB21:    goto LAB20;
+
+LAB23:    goto LAB21;
+
+LAB24:    goto LAB2;
+
+LAB25:    goto LAB24;
+
+LAB27:    goto LAB25;
+
+}
+
+
+extern void work_a_1773838443_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_1773838443_2372691052_p_0};
+	xsi_register_didat("work_a_1773838443_2372691052", "isim/Ripple_Adder_tb_isim_beh.exe.sim/work/a_1773838443_2372691052.didat");
+	xsi_register_executes(pe);
+}
