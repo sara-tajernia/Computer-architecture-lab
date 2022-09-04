91e5c822dfa7c44ad95dbc91c901de2a53304cf1
Sara Tajernia <61985583+sara-tajernia@users.noreply.github.com>
2022-09-04 09:37:01 +0430

    Add files via upload

---
+++ Lab4/az4/isim/Tb_Carry_Look_Ahead_isim_beh.exe.sim/work/a_0809384954_2372691052.c
@@ -0,0 +1,180 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "S:/uni/4/Az memar/session4/az4/CL_tb.vhd";
+
+
+
+static void work_a_0809384954_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    int64 t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+
+LAB0:    t1 = (t0 + 2832U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(44, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2640);
+    xsi_process_wait(t2, t3);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(46, ng0);
+    t2 = (t0 + 5178);
+    t5 = (t0 + 3216);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(47, ng0);
+    t2 = (t0 + 5182);
+    t5 = (t0 + 3280);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(48, ng0);
+    t2 = (t0 + 3344);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    *((unsigned char *)t7) = (unsigned char)3;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(50, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2640);
+    xsi_process_wait(t2, t3);
+
+LAB10:    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+LAB8:    xsi_set_current_line(52, ng0);
+    t2 = (t0 + 5186);
+    t5 = (t0 + 3216);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(53, ng0);
+    t2 = (t0 + 5190);
+    t5 = (t0 + 3280);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(54, ng0);
+    t2 = (t0 + 3344);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    *((unsigned char *)t7) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(56, ng0);
+    t3 = (10 * 1000LL);
+    t2 = (t0 + 2640);
+    xsi_process_wait(t2, t3);
+
+LAB14:    *((char **)t1) = &&LAB15;
+    goto LAB1;
+
+LAB9:    goto LAB8;
+
+LAB11:    goto LAB9;
+
+LAB12:    xsi_set_current_line(58, ng0);
+    t2 = (t0 + 5194);
+    t5 = (t0 + 3216);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(59, ng0);
+    t2 = (t0 + 5198);
+    t5 = (t0 + 3280);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t2, 4U);
+    xsi_driver_first_trans_fast(t5);
+    xsi_set_current_line(60, ng0);
+    t2 = (t0 + 3344);
+    t4 = (t2 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    *((unsigned char *)t7) = (unsigned char)2;
+    xsi_driver_first_trans_fast(t2);
+    xsi_set_current_line(62, ng0);
+
+LAB18:    *((char **)t1) = &&LAB19;
+    goto LAB1;
+
+LAB13:    goto LAB12;
+
+LAB15:    goto LAB13;
+
+LAB16:    goto LAB2;
+
+LAB17:    goto LAB16;
+
+LAB19:    goto LAB17;
+
+}
+
+
+extern void work_a_0809384954_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_0809384954_2372691052_p_0};
+	xsi_register_didat("work_a_0809384954_2372691052", "isim/Tb_Carry_Look_Ahead_isim_beh.exe.sim/work/a_0809384954_2372691052.didat");
+	xsi_register_executes(pe);
+}
